51fe889836ec8a764dcae130695a6b2af8e4fd05
Shreerang Vaidya <shreerangvaidya28@gmail.com>
2023-10-07 22:35:14 +0530

    ml submodule

---
+++ Makefile
@@ -0,0 +1,28 @@
+SHELL = /bin/bash
+
+default: runall
+
+setup:
+	@echo "------ setting up vite ------"
+	@cd vite && npm i
+	@echo "------- entering venv -------"
+	@source bin/activate
+	@echo "-- installing requirements --"
+	@pip install -r requirements.txt -r ml/requirements.txt
+	@echo "----------- done! -----------"
+
+build:
+	@cd vite && npm run build
+	@cd ml && make build
+
+run:
+	@python app.py
+
+# runall doesn't run setup
+# the name runall is a lie
+runall:
+	@make build && make run
+
+update:
+	@git submodule update
+	@cd vite && npm i
--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask
 from flask import render_template
+from pathlib import Path
 import db
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder=Path(".") / "dist",
+    static_folder=Path(".") / "dist" / "assets",
+    static_url_path="/assets",
+)
 
 
 @app.route("/")
-# TODO: if logged in make this conditional
-def landing_or_dashboard():
-    return "TODO"
+def landing():
+    return render_template("index.html")
 
 
 @app.route("/dashboard")
@@ -20,3 +25,6 @@ def dashboard():
 # this is just an example so I don't forget what TODO returns have to be replaced with later on
 def TODO_filler(file_loc: str):
     return render_template(file_loc)
+
+
+app.run(debug=True)
